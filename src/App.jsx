b95aec0d556b6638708e53029708536e6dfe97e4
chaman-gautam <chaman12gautam12@gmai.com>
2025-09-10 17:42:20 +0530

    try to resolve error

--- src/App.jsx
+++ src/App.jsx
@@ -1,99 +1,4 @@
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import Home from "./pages/Home";
-// import Aboutme from "./components/Aboutme";
-// import Pagenotfound from "./pages/Pagenotfound";
-// import Contact from "./components/Contact";
-// import Navbar from "./components/Navbar";
-// import Footer from "./components/Footer";
-// import CaseStudies from "./pages/Casestudy";
-// import CaseStudyDetail from "./pages/Casestudydetail1";
-
-// import Leadership from "./components/Leadership";
-// import Professionalroles from "./components/Services";
-// import Singlepage from "./pages/Singlepage";
-// import Resume from "./pages/Resume";
-// export default function App() {
-//   return (
-//     <BrowserRouter basename="/portfolio_port">
-//       {/* navbar  */}
-//       <div className="flex items-center justify-center sticky top-0 z-50 ">
-//         <Navbar />
-//       </div>
-//       <Routes>
-//         {/* <Route index element={<Home />} /> */}
-//         <Route path="/" element={<Home />} />
-
-//         {/* <Route path="/Aboutme" element={<Aboutme />} /> */}
-//         <Route path="/contact" element={<Contact />} />
-//         <Route
-//           path="/aboutme"
-//           element={
-//             <Singlepage>
-//               <Aboutme />
-//             </Singlepage>
-//           }
-//         />
-//         <Route path="/casestudy" element={<CaseStudies />} />
-//         <Route
-//           path="/professionalroles"
-//           element={
-//             <Singlepage>
-//               <Professionalroles />
-//             </Singlepage>
-//           }
-//         />
-//         <Route
-//           path="/leadership"
-//           element={
-//             <Singlepage>
-//               <Leadership />
-//             </Singlepage>
-//           }
-//         />
-//         <Route
-//           path="/Ceostrategy"
-//           element={
-//             <Singlepage>
-//               <Leadership />
-//             </Singlepage>
-//           }
-//         />
-//         <Route path="/resume" element={<Resume />} />
-//         <Route
-//           path="/Founderjourney"
-//           element={
-//             <Singlepage>
-//               <Leadership />
-//             </Singlepage>
-//           }
-//         />
-
-//         <Route path="/casestudydetail/:id" element={<CaseStudyDetail />} />
-//         <Route path="*" element={<Pagenotfound />} />
-//       </Routes>
-//       <div>
-//         <Footer />
-//       </div>
-//     </BrowserRouter>
-//   );
-// }
-
-// // import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-// // export default function App() {
-// //   return (
-// //     <BrowserRouter>
-// //       <Routes>
-// //         <Route
-// //           path="/"
-// //           element={<h1 className="text-blue-600">Hello inline</h1>}
-// //         />
-// //       </Routes>
-// //     </BrowserRouter>
-// //   );
-// // }
-
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Aboutme from "./components/Aboutme";
 import Pagenotfound from "./pages/Pagenotfound";
@@ -102,23 +7,24 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import CaseStudies from "./pages/Casestudy";
 import CaseStudyDetail from "./pages/Casestudydetail1";
+
 import Leadership from "./components/Leadership";
 import Professionalroles from "./components/Services";
 import Singlepage from "./pages/Singlepage";
 import Resume from "./pages/Resume";
-
 export default function App() {
   return (
-    <>
-      {/* navbar */}
+    <BrowserRouter basename="/portfolio_port">
+      {/* navbar  */}
       <div className="flex items-center justify-center sticky top-0 z-50 ">
         <Navbar />
       </div>
-
       <Routes>
+        {/* <Route index element={<Home />} /> */}
         <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
 
+        {/* <Route path="/Aboutme" element={<Aboutme />} /> */}
+        <Route path="/contact" element={<Contact />} />
         <Route
           path="/aboutme"
           element={
@@ -127,9 +33,7 @@ export default function App() {
             </Singlepage>
           }
         />
-
         <Route path="/casestudy" element={<CaseStudies />} />
-
         <Route
           path="/professionalroles"
           element={
@@ -138,7 +42,6 @@ export default function App() {
             </Singlepage>
           }
         />
-
         <Route
           path="/leadership"
           element={
@@ -147,7 +50,6 @@ export default function App() {
             </Singlepage>
           }
         />
-
         <Route
           path="/Ceostrategy"
           element={
@@ -156,9 +58,7 @@ export default function App() {
             </Singlepage>
           }
         />
-
         <Route path="/resume" element={<Resume />} />
-
         <Route
           path="/Founderjourney"
           element={
@@ -169,12 +69,26 @@ export default function App() {
         />
 
         <Route path="/casestudydetail/:id" element={<CaseStudyDetail />} />
-
-        {/* Catch-all */}
         <Route path="*" element={<Pagenotfound />} />
       </Routes>
-
-      <Footer />
-    </>
+      <div>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
+
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route
+//           path="/"
+//           element={<h1 className="text-blue-600">Hello inline</h1>}
+//         />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
